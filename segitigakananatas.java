46ee15d38460c8d8f04c97034d324b6256e78b71
EmaKarisna <Karisnaema@gmail.com>
2016-11-03 23:41:19 +0700

    Add files via upload

---
+++ segitigakananatas.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package segitiga;
+
+/**
+ *
+ * @author setyakarisna
+ */
+public class segitigakananatas {//untuk membuat judul pada class
+   public static void main(String[] args) {
+            int x =6;
+            for (int i=1;i<=6;i++){//digunakan untuk melakukan perulangan yang menghasilkan baris dari pola tersebut dimana perulangan dimulai dari sampai masukan yang telah di masukkan sebelumnya.
+            for(int j=6;j>=i;j--){//untuk mengatur spasi antar bintang pada setiap barisnya
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){//digunakan untuk melakukan perulangan yang menghasilkan (*) dalam pembuatan pola 
+                System.out.print("*");//simbol yang digunakan untuk mengisi pola
+            }
+            System.out.println();//Mencetak hasil
+      }
+            
+   }
+}
---
+++ segitigasamasisi.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package segitiga;
+
+/**
+ *
+ * @author setyakarisna
+ */
+public class segitigasamasisi {//untuk membuat judul pada class
+    public static void main (String[] args) {
+        int x = 6;
+            for(int i=1; i<=x; i++) {//digunakan untuk melakukan perulangan yang menghasilkan baris dari pola tersebut dimana perulangan dimulai dari sampai masukan yang telah di masukkan sebelumnya.
+            for(int j=x; j>i; j--) {//untuk mengatur spasi antar bintang pada setiap barisnya
+              System.out.print(" ");
+        }
+            for(int k=1; k<=i; k++) {//digunakan untuk melakukan perulangan yang menghasilkan (*) dalam pembuatan pola 
+              System.out.print("* ");//simbol yang digunakan untuk mengisi pola
+        }
+             System.out.println();//Mencetak hasil
+        }
+            System.out.println();
+            System.out.print("baris = " + x);
+        }
+    }
+ 
